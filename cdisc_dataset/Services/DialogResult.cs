0a62b5346e2db50ada6847dd83d46573291c270f
zhi <15000860686@163.com>
2026-08-19 11:45:42 +0800

    update

---
+++ cdisc_dataset/Services/DialogResult.cs
@@ -0,0 +1,28 @@
+﻿using AsyncNavigation.Abstractions;
+using AsyncNavigation.Core;
+
+namespace cdisc_dataset.Services;
+
+public sealed class DialogHostResult : IDialogResult
+{
+    public DialogHostResult()
+    {
+    }
+
+    public DialogHostResult(DialogButtonResult result)
+    {
+        Result = result;
+    }
+
+    public DialogHostResult(DialogButtonResult result, IDialogParameters parameters)
+    {
+        Result = result;
+        Parameters = parameters;
+    }
+
+    public IDialogParameters? Parameters { get; set; } = new DialogParameters();
+
+    public DialogButtonResult Result { get; set; }
+
+    public DialogStatus Status { get; set; } = DialogStatus.Closed;
+}
