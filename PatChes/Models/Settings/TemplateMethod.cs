0a067b2fab25ee8cd90fc836d372cd6f38d56a3a
zhi <15000860686@163.com>
2026-08-22 19:56:15 +0800

    1

---
+++ PatChes/Models/Settings/TemplateMethod.cs
@@ -0,0 +1,32 @@
+﻿using PatChes.Models.Enums;
+using SqlSugar;
+
+namespace PatChes.Models.Settings;
+
+[Tenant("setting")]
+[SugarTable("template_method")]
+public class TemplateMethod
+{
+    [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
+    public int Id { get; set; }
+
+    [SugarColumn(IsNullable = true)]
+    public string? UniqueId { get; set; }
+
+    [SugarColumn(IsNullable = true)]
+    public string? Name { get; set; }
+
+    [SugarColumn(IsNullable = true)]
+    public string? Type { get; set; }
+
+    [SugarColumn(IsNullable = true)]
+    public string? Description { get; set; }
+
+    [SugarColumn(IsNullable = true)]
+    public string? ExpressionContext { get; set; }
+
+    [SugarColumn(IsNullable = true)]
+    public string? ExpressionCode { get; set; }
+
+    public CdiscDataType CdiscDataType { get; set; }
+}
